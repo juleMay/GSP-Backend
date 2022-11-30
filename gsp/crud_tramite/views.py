fd4161d79eb864b51ea32199ac2de99e4b8a59e6
jandresordonez <jandresordonez@unicauca.edu.co>
2022-11-30 15:40:27 -0500

    Endpoints para metodos GET individual y de lista de Tramites

--- gsp/crud_tramite/views.py
+++ gsp/crud_tramite/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import *
+from .models import Tramite
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def tramite_list(request):
+    """
+    POST: Retorna la lista de elementos del modelo Tramites en formato Json.
+    GET: Guarda nuevas entradas del modelo Tramites.
+    """
+    if request.method == 'GET':
+        tramites = Tramite.objects.all()
+        serializer = TramtiteSerializer(tramites, many=True)
+        return Response({"tramites": serializer.data}, status=status.HTTP_200_OK)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def tramite_detail(request, id):
+    """
+    GET: Retorna un individuo del modelo Tramites en formato Json con pk = id.
+    PUT: Modifica un individuo del modelo Tramites con pk = id.
+    DELETE: Elimina un individuo del modelo Tramites con pk = id.
+    """
+    try:
+        tramite = Tramite.objects.get(pk=id)
+    except Tramite.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = TramtiteSerializer(tramite)
+        return Response(serializer.data)
--- gsp/gsp/urls.py
+++ gsp/gsp/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from crud_tramite import views
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+    path('tramites/', views.tramite_list),
+    path('tramites/<int:id>', views.tramite_detail),
 ]
